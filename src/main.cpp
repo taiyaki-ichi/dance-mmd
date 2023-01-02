26b48e1583278e7e47148c2bb0e250bd8aa26ee0
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2023-01-02 14:18:09 +0900

    ウィンドウのの作成

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
+#include"../external/directx12-wrapper/dx12w/dx12w.hpp"
+
 
 int main()
 {
+	constexpr LONG widnow_width = 1024;
+	constexpr LONG window_height = 768;
 
+	auto window = dx12w::create_window(L"dance-mmd", widnow_width, window_height);
+
+	while (dx12w::update_window())
+	{
+
+	}
+
+	return 0;
 }

5e837a234a0b7649b66b361eb93fecfd07dda82c
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2023-01-03 13:42:15 +0900

    imguiの導入

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,37 @@
 #include"../external/directx12-wrapper/dx12w/dx12w.hpp"
-
+#include"../external/imgui/imgui.h"
+#include"../external/imgui/imgui_impl_dx12.h"
+#include"../external/imgui/imgui_impl_win32.h"
 
 constexpr LONG WINDOW_WIDTH = 1024;
 constexpr LONG WINDOW_HEIGHT = 768;
 
 constexpr std::size_t COMMAND_ALLOCATOR_NUM = 1;
 
-constexpr DXGI_FORMAT FRAME_BUFFER_FORMAT = DXGI_FORMAT_B8G8R8A8_UNORM;
+constexpr DXGI_FORMAT FRAME_BUFFER_FORMAT = DXGI_FORMAT_R8G8B8A8_UNORM;
 constexpr std::size_t FRAME_BUFFER_NUM = 2;
 
 constexpr DXGI_FORMAT DEPTH_BUFFER_FORMAT = DXGI_FORMAT_D32_FLOAT;
 
+// extern宣言
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+LRESULT CALLBACK wnd_proc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	// Imgui用の処理
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+		return true;
+	
+	if (msg == WM_DESTROY) {
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int main()
 {
-	auto hwnd = dx12w::create_window(L"dance-mmd", WINDOW_WIDTH, WINDOW_HEIGHT);
+	auto hwnd = dx12w::create_window(L"dance-mmd", WINDOW_WIDTH, WINDOW_HEIGHT, wnd_proc);
 
 	auto device = dx12w::create_device();
 	auto command_manamger = dx12w::create_command_manager<COMMAND_ALLOCATOR_NUM>(device.get());
@@ -64,6 +82,27 @@ int main()
 			dx12w::create_texture2D_RTV(device.get(), frame_buffer_descriptor_heap_RTV.get_CPU_handle(i), frame_buffer_resource[i].first.get(), FRAME_BUFFER_FORMAT, 0, 0);
 	}
 
+	// imgui用のディスクリプタヒープ
+	dx12w::descriptor_heap imgui_descriptor_heap{};
+	imgui_descriptor_heap.initialize(device.get(), D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, 1);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsLight();
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplWin32_Init(hwnd);
+	ImGui_ImplDX12_Init(device.get(), 1,
+		DXGI_FORMAT_R8G8B8A8_UNORM, imgui_descriptor_heap.get(),
+		imgui_descriptor_heap.get_CPU_handle(),
+		imgui_descriptor_heap.get_GPU_handle());
 
 
 	while (dx12w::update_window())
@@ -72,15 +111,47 @@ int main()
 
 		command_manamger.reset_list(0);
 
+		//
+		// Imguiの準備
+		//
+
+		// Start the Dear ImGui frame
+		ImGui_ImplDX12_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+
+		bool hoge = true;
+		ImGui::ShowDemoWindow(&hoge);
+
+		ImGui::Begin("Hello, world!");
+		ImGui::Text("This is some useful text.");
+		ImGui::End();
+
+		// Rendering
+		ImGui::Render();
+
+		//
+		//
+		//
+
 		command_manamger.get_list()->RSSetViewports(1, &viewport);
 		command_manamger.get_list()->RSSetScissorRects(1, &scissor_rect);
 
 		dx12w::resource_barrior(command_manamger.get_list(), frame_buffer_resource[back_buffer_index], D3D12_RESOURCE_STATE_RENDER_TARGET);
 		command_manamger.get_list()->ClearRenderTargetView(frame_buffer_descriptor_heap_RTV.get_CPU_handle(back_buffer_index), frame_buffer_clear_value.Color, 0, nullptr);
 
+		auto frame_buffer_cpu_handle = frame_buffer_descriptor_heap_RTV.get_CPU_handle(back_buffer_index);
+		command_manamger.get_list()->OMSetRenderTargets(1, &frame_buffer_cpu_handle, false, nullptr);
+
 		//
+		// フレームへの描画
 		//
-		//
+
+		// Imguiの描画
+		auto imgui_descriptor_heap_ptr = imgui_descriptor_heap.get();
+		command_manamger.get_list()->SetDescriptorHeaps(1, &imgui_descriptor_heap_ptr);
+		ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), command_manamger.get_list());
+
 
 		dx12w::resource_barrior(command_manamger.get_list(), frame_buffer_resource[back_buffer_index], D3D12_RESOURCE_STATE_COMMON);
 
@@ -92,6 +163,11 @@ int main()
 
 		swap_chain->Present(1, 0);
 	}
+
+
+	ImGui_ImplDX12_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
 
 	return 0;
 }
